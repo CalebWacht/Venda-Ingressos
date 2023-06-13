dec0378a2a729e8a4a9db230aea48f22cc4c28d4
Caleb Wacht <calebdasilveira@gmail.com>
2023-06-12 21:45:03 -0300

    Commit Inicial

--- main.c
+++ main.c
@@ -1,53 +1,65 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main(void) {
-    
-    int qtdRuim, qtdRegular, qtdBom, qtdOtimo, opcao;
-    
-    qtdRuim = 0;
-    qtdRegular = 0;
-    qtdBom = 0;
-    qtdOtimo = 0;
-    
-    do{ 
-        printf("\nO que voce achou do antendimento? Digite uma opcao\n");
-        printf("0 - RUIM\n");
-        printf("1 - REGULAR\n");
-        printf("2 - BOM\n");
-        printf("3 - OTIMO\n");
-        printf("4 - SAIR\n");
-        
-        scanf("%d", &opcao);
-        
-        switch (opcao){
-               
-           case 0:
-                 qtdRuim = qtdRuim + 1;   
-                 break;
-           case 1:
-                 qtdRegular = qtdRegular + 1;
-                 break;
-           case 2:
-                 qtdBom = qtdBom + 1;
-                 break;
-           case 3:
-                 qtdOtimo = qtdOtimo + 1;
-                 break;
-           case 4:
-                break;
-           default:
-                 printf("Opcao invalida, por favor selecione novamente");   
-                 break;
-        }
-        
-    } while (opcao != 4);
+struct  lista
+{
+      char nome[20];
+      int anoNascimento;
+      struct lista *proximo;
+};
+
+      struct lista m1, m2, m3;
+      struct lista *gancho = &m1;
       
+
+int main(void)
+{     
+
+      int opcao;
+
       system("cls");
-      printf("Quantidade de Votos no Ruim: %i\n", qtdRuim);
-      printf("Quantidade de Votos no Regular: %i\n", qtdRegular);
-      printf("Quantidade de Votos no Bom: %i\n", qtdBom);
-      printf("Quantidade de Votos no Otimo: %i\n", qtdOtimo);
+      printf("===============================\n");
+      printf("Bem vindo ao Circo Maracutaia!\n");
+      printf("===============================\n\n");
+
+      printf("Selecionar Opção ");
+      printf("\n 1- Vender Ingresso");
+      printf("\n 2- Listar ingressos Vendidos\n");
+      printf("\n 3- Validar ingresso\n");
+      printf("\n 4- Sair\n");
+      scanf("%d", &opcao);
+
+      switch (opcao)
+      {
+      case 1:
+            opcaoVender();
+            break;
+      case 2:
+            opcaoListar();
+            break;
+
+      case 3:
+            opcaoValidar();
+            break;
+
+      case 4:
+            break;
+
+      default:
+            printf("Opcao Invalida");
+      }
+}
+
+void opcaoVender(){
+      printf("===============================\n");
+      printf("Preencha com as suas informações: \n");
       
-return 0;
+      printf("Nome: \n");
+            
+      printf("Ano Nascimento: \n");
+               
+}
+
+void opcaoListar(){
+
 }
